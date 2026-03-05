67f30c3508fd905d3f6cb8a552c0ffac7376388d
Valtyr <cyriel.bastin@zoho.com>
2026-03-05 15:28:42 +0100

    regenerate .git directory

---
+++ index.js
@@ -0,0 +1,4 @@
+export { createApp } from './lib/app.js'
+export { defineComponent } from './lib/component.js'
+export { DOM_TYPES, h, hFragment, hString } from './lib/h.js'
+export { nextTick } from './lib/scheduler.js'
---
+++ vitest.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        include: ['./tests/**/*.test.js'],
+        reporters: 'verbose',
+        environment: 'jsdom',
+        coverage: {
+            provider: 'v8'
+        }
+    }
+})
